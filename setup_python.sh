39429e57ce3c916f439e7ea91661b16c49012650
Yogesh Swami <axelexic@gmail.com>
2025-10-05 22:07:02 -0700

    Move to Sage 10.7
    Move to Sage 10.7

--- setup_python.sh
+++ setup_python.sh
@@ -5,7 +5,7 @@ SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 VENV_DIR_NAME="$SCRIPT_DIR/venv"
 VENV_PROMPT_NAME=$(basename $SCRIPT_DIR)
 
-SAGE="$HOME/Applications/SageMath-10-6.app/Contents/Frameworks/Sage.framework/Versions/10.6/local/var/lib/sage/venv-python3.12.5/bin/sage"
+SAGE="$HOME/Applications/SageMath-10-7.app/Contents/Frameworks/Sage.framework/Versions/10.7/local/bin/sage"
 
 if [ -d "$VENV_DIR_NAME" ]; then
   echo "Virtual environment \"$VENV_DIR_NAME\" already exists"
